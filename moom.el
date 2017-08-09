3d4d064fb805f49585e4c004f40ba26167357726
Takaaki ISHIKAWA <takaxp@ieee.org>
2017-08-10 08:34:42 +0900

    Checked by package-lint

--- moom.el
+++ moom.el
@@ -1,25 +1,25 @@
-;;; moom.el --- A tool to control frame size, position, and font size
-;;
+;;; moom.el --- Commands to control frame size and position
+
 ;; Copyright (C) 2017 Takaaki ISHIKAWA
-;;
+
 ;; Author: Takaaki ISHIKAWA <takaxp at ieee dot org>
-;; Keywords: frame-cmds, frame, size, position
+;; Keywords: frames, faces, convenience
 ;; Version: 0.9.0
 ;; Maintainer: Takaaki ISHIKAWA <takaxp at ieee dot org>
 ;; URL: https://github.com/takaxp/Moom
 ;; Package-Requires: ((frame-cmds "0"))
 ;; Twitter: @takaxp
-;;
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
